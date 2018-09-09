1da239d2fab08fecbbddb8f98e34c98c1ede60a2
cruelka <kes1993@ya.ru>
2018-09-09 16:50:45 +0400

    nav

---
+++ resources/views/model.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.head')
+
+@section('content')
+<div id="page-title" class="page-title page-title-1">
+    <div class="bg-overlay"></div>                    <div class="container">
+        <div class="row">
+            <div id="page-title-text" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 "><h1>Winifred Hunt</h1></div>
+            <div id="breadcrumb-text" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 "><!-- Breadcrumb NavXT 5.7.1 -->
+                <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to Burnley." href="http://demo.spyropress.com/themeforest/burnley" class="home"><span property="name">Burnley</span></a><meta property="position" content="1"></span> &gt; <span property="itemListElement" typeof="ListItem"><span property="name"><span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to the Top Model Category archives." href="http://demo.spyropress.com/themeforest/burnley/archives/model_category/top-model" class="model_category"><span property="name">Top Model</span></a><meta property="position" content="2"></span></span><meta property="position" content="2"></span> &gt; <span property="itemListElement" typeof="ListItem"><span property="name">Winifred Hunt</span><meta property="position" content="3"></span></div>
+        </div>
+    </div>
+</div><!-- #page-title -->
+<!-- #page-title -->
+<div id="content" class="site-content "><div id="primary" class="style-model ds section_padding_100 columns_padding_25">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-5 col-left-media">
+                    <div class="vertical-item content-padding text-center">
+                        <div class="item-media">
+                            <img width="700" height="1052" src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/08/04.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" />		</div>
+                        <div class="item-content transp_gradient_bg">
+                            <h3 class="model-name">
+                                <a href="#">Winifred Hunt</a>
+                            </h3>
+                            <p class="small-text lusitana highlight"><a href="http://demo.spyropress.com/themeforest/burnley/archives/model_category/top-model" rel="tag">Top Model</a></p>
+
+                            <div class="fontsize_16 lusitana text-uppercase grey parameters">
+				<span>
+					Height					<br>
+					<span class="highlight">
+						185					</span>
+				</span>
+				<span>
+					Bust					<br>
+					<span class="highlight">
+						79					</span>
+				</span>
+				<span>
+					Waist					<br>
+					<span class="highlight">
+						59					</span>
+				</span>
+				<span>
+					Hips					<br>
+					<span class="highlight">
+						87					</span>
+				</span>
+				<span>
+					Shoes					<br>
+					<span class="highlight">
+						39					</span>
+				</span>
+				<span>
+					Eyes					<br>
+					<span class="highlight">
+						Green					</span>
+				</span>
+				<span>
+					Hair					<br>
+					<span class="highlight">
+						Brown					</span>
+				</span>
+				<span>
+					Size					<br>
+					<span class="highlight">
+						8					</span>
+				</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-7 col-right-text">
+                    <p>
+                    <h3><span style="color: #ffffff;">About Model</span></h3>
+                    Drumstick pig ham hock brisket pork chop, pastrami beef rump ham pancetta cow. Prosciutto sirloin t-bone tri-tip, meatloaf landjaeger rump doner ham hock tongue swine salami shoulder beef ribs. Pork chop ball tip short ribs pork, tri-tip sirloin corned beef. Andouille alcatra pancetta tongue pork loin ribeye bresaola sirloin doner corned beef hamburger tenderloin.
+
+
+                    Turducken fatback andouille, bacon pork loin ham hock ribeye salami kevin pancetta. Jerky ham pork belly sirloin. Cow short loin porchetta doner, leberkas chicken pork boudin venison beef tenderloin frankfurter. Ribeye drumstick strip steak shankle brisket chicken picanha short loin beef pork chop. Chicken beef ribs beef flank.
+                    </p>
+                    <div class="media small-teaser grey">
+                        <div class="media-left">
+                            <i class="fa fa-globe highlight2"></i>
+                        </div>
+                        <div class="media-body">
+                            Jacksotts str. 698 San Diego, California, USA		</div>
+                    </div>
+
+                    <div class="media small-teaser grey">
+                        <div class="media-left">
+                            <i class="fa fa-phone highlight2"></i>
+                        </div>
+                        <div class="media-body">
+                            1-800-123-4567, 1-800-123-4568		</div>
+                    </div>
+
+                    <div class="media small-teaser grey">
+                        <div class="media-left">
+                            <i class="fa fa-envelope highlight2"></i>
+                        </div>
+                        <div class="media-body underlined-links">
+                            <a href="mailto:your@mail.com">
+                                rachel_tate@support.com			</a>
+                        </div>
+                    </div>
+                    <!-- Gallery -->
+                    <div class="row model-gallery-image"><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/08.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/08.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/07.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/07.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/06.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/06.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/04.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/04.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/05.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/05.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/03.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/03.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/02.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/02.jpg"><i class="fa fa-search"></i></a></div></div><div class="col-xs-6 col-sm-4 col-md-4 col-lg-3 image-col"><img src="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/01.jpg" alt="" /><div class="media-links"><a class="magic-popups" title="" href="http://demo.spyropress.com/themeforest/burnley/wp-content/uploads/2017/09/01.jpg"><i class="fa fa-search"></i></a></div></div></div>	<!-- form contact -->
+                    <div class="model-contact-form">
+                        <h3>Contact model</h3>
+                        <div class="form-model">
+                            <div role="form" class="wpcf7" id="wpcf7-f473-p488-o1" lang="en-US" dir="ltr">
+                                <div class="screen-reader-response"></div>
+                                <form action="/themeforest/burnley/archives/model/winifred-hunt#wpcf7-f473-p488-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                                    <div style="display: none;">
+                                        <input type="hidden" name="_wpcf7" value="473" />
+                                        <input type="hidden" name="_wpcf7_version" value="4.9.1" />
+                                        <input type="hidden" name="_wpcf7_locale" value="en_US" />
+                                        <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f473-p488-o1" />
+                                        <input type="hidden" name="_wpcf7_container_post" value="488" />
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="FULL NAME" /></span>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="SUBJECT" /></span>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <span class="wpcf7-form-control-wrap your-phone"><input type="text" name="your-phone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="PHONE NUMBER" /></span>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="EMAIL ADDRESS" /></span>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                            <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="MESSAGE"></textarea></span>
+                                        </div>
+                                        <div class="col-xs-12 add-submit">
+                                            <input type="submit" value="Submit" class="wpcf7-form-control wpcf7-submit" />
+                                        </div>
+                                    </div>
+                                    <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>		</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div><!-- #primary -->
+</div><!-- .site-content -->
+    @endsection
