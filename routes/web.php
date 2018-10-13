c0c5050016c9592403b1d60ec7e04a541baced38
root <root@hostname.orchid.live>
2018-10-14 00:20:05 +0200

    wefwef

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/model', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
