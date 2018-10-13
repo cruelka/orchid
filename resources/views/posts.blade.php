17f23ff02aa99e4a44891024444dc3d850aa743e
cruelka <kes1993@ya.ru>
2018-10-14 03:04:20 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.head')
+
+@section('content')
+
+    <div class="container">
+
+        <div class="news container">
+            <div class="row">
+                @foreach($posts as $post)
+
+
+                    <div class="col-sx-12 col-md-6">
+                        <div class="news__item">
+
+                            <!--<div class="col-lg-2">
+                            {{ \Carbon\Carbon::parse($post->created_at)->format('d/M/y')}}
+                                    </div>-->
+                            <div class="">
+                                <div class="news__item-inner">
+
+                                    <a href="/post/{{ $post->slug }}" class="news-card__img-wrapper">
+                                        <div class="news-card__img" style="background: url('/storage/{{$post->image}}')">
+                                        </div>
+                                    </a>
+
+                                    <div class="news-card__lable">
+                                        {{ $post->meta_keywords }}
+                                    </div>
+                                    <a href="/post/{{ $post->slug }}" class="news-card__title">
+                                        {{ $post->title }}
+                                    </a>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                @endforeach
+                {{ $posts->links() }}
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
-Route::get('page/{slug}', function($slug){
+Route::get('/page/{slug}', function($slug){
     $data = App\Page::where('slug', '=', $slug)->firstOrFail();
     $posts = App\Post::take(3)->get();
     return view('content', compact('data','posts'));
 });
 
 
-Route::get('post/{slug}', function($slug){
+Route::get('/post/{slug}', function($slug){
     $post = App\Post::where('slug', '=', $slug)->firstOrFail();
     return view('post', compact('post'));
+});
+Route::get(/'blog', function () {
+    $posts = App\Post::paginate(3);
+    return view('posts', compact('posts'));
 });
