5c9a6cec398ba8fb7dcd5e9e25be21f1b23aa17a
cruelka <kes1993@ya.ru>
2018-10-14 03:02:37 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

---
+++ resources/views/post.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.head')
+
+@section('content')
+    <div class=" container">
+        <div class="news container">
+            <div class="news__item">
+                <div class="row">
+                    <div class="col-lg-2">
+                        {{ \Carbon\Carbon::parse($post->created_at)->format('d/M/y')}}
+                    </div>
+                    <div class="col-lg-10">
+                        <div class="news__item-inner">
+                            <img class="news__item-img" src="/storage/{{$post->image}}" alt="Image">
+
+
+                            <div class="news-card__title">
+                                {{$post->title}}
+                            </div>
+
+                            <div class="news-item__body">
+                                {!! $post->body !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
