6ba216664fbbba7ff6ec2b97a97a4f587ba2933b
cruelka <kes1993@ya.ru>
2018-10-18 07:58:52 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

--- app/Photo.php
+++ app/Photo.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+
+    protected $table ='photo';
+
     protected $fillable = [
         'name', 'user_id'
     ];
+
     
 }
