b016c6d68a590e1ea8276b06c2a8475d0c000958
cruelka <kes1993@ya.ru>
2018-10-18 07:24:27 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,14 +16,14 @@
                                     <div class="panel panel-default">
 
                                         <div class="panel-body">
-                                            <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                                            <form class="form-horizontal" method="POST" action="">
                                                 {{ csrf_field() }}
 
                                                 <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                                     <label for="name" class="col-md-4 control-label">Name</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                                        <input id="name" type="text" class="form-control" name="name" value="{{Auth::user()->name }}" required autofocus>
 
                                                         @if ($errors->has('name'))
                                                             <span class="help-block">
@@ -37,7 +37,7 @@
                                                     <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                                        <input id="email" type="email" class="form-control" name="email" value="{{Auth::user()->email }}" required>
 
                                                         @if ($errors->has('email'))
                                                             <span class="help-block">
@@ -47,32 +47,13 @@
                                                     </div>
                                                 </div>
 
+
                                                 <div class="form-group">
                                                     <label for="password" class="col-md-4 control-label">Phone</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Phone" type="text" class="form-control" name="phone" required>
+                                                        <input id="Phone" type="text" class="form-control" value ="{{Auth::user()->phone }}"name="phone" required>
 
-                                                    </div>
-                                                </div>
-                                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                                    <label for="password" class="col-md-4 control-label">Password</label>
-
-                                                    <div class="col-md-6">
-                                                        <input id="password" type="password" class="form-control" name="password" required>
-
-                                                        @if ($errors->has('password'))
-                                                            <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                                        @endif
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                                                    <div class="col-md-6">
-                                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                                                     </div>
                                                 </div>
 
@@ -80,7 +61,7 @@
                                                     <label for="password" class="col-md-4 control-label">Height</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="height" type="text" class="form-control" name="height" required>
+                                                        <input id="height" type="text" class="form-control" value ="{{Auth::user()->height }}"name="height" required>
 
                                                     </div>
                                                 </div>
@@ -88,7 +69,7 @@
                                                     <label for="password" class="col-md-4 control-label">Waist</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Waist" type="text" class="form-control" name="waist" required>
+                                                        <input id="Waist" type="text" class="form-control" value ="{{Auth::user()->waist }}" name="waist" required>
 
                                                     </div>
                                                 </div>
@@ -96,7 +77,7 @@
                                                     <label for="password" class="col-md-4 control-label">Bust</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Bust" type="text" class="form-control" name="bust" required>
+                                                        <input id="Bust" type="text" class="form-control" value ="{{Auth::user()->bust }}" name="bust" required>
 
                                                     </div>
                                                 </div>
@@ -104,7 +85,7 @@
                                                     <label for="password" class="col-md-4 control-label">Hips</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Hips" type="text" class="form-control" name="hips" required>
+                                                        <input id="Hips" type="text" class="form-control"value ="{{Auth::user()->hips }}"  name="hips" required>
 
                                                     </div>
                                                 </div>
@@ -112,7 +93,7 @@
                                                     <label for="password" class="col-md-4 control-label">Shoes</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Shoes" type="text" class="form-control" name="shoes" required>
+                                                        <input id="Shoes" type="text" class="form-control" value ="{{Auth::user()->shoes }}"  name="shoes" required>
 
                                                     </div>
                                                 </div>
@@ -120,7 +101,7 @@
                                                     <label for="password" class="col-md-4 control-label">Eyes</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="eyes" type="text" class="form-control" name="eyes" required>
+                                                        <input id="eyes" type="text" class="form-control" value ="{{Auth::user()->eyes }}"  name="eyes" required>
 
                                                     </div>
                                                 </div>
@@ -128,7 +109,7 @@
                                                     <label for="password" class="col-md-4 control-label">Hair</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Hair" type="text" class="form-control" name="hair" required>
+                                                        <input id="Hair" type="text" class="form-control" value ="{{Auth::user()->hair }}"  name="hair" required>
 
                                                     </div>
                                                 </div>
@@ -136,7 +117,7 @@
                                                     <label for="password" class="col-md-4 control-label">Size</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="Size" type="text" class="form-control" name="size" required>
+                                                        <input id="Size" type="text" class="form-control" value ="{{Auth::user()->size }}" name="size" required>
 
                                                     </div>
                                                 </div>
@@ -145,7 +126,7 @@
                                                 <div class="form-group">
                                                     <div class="col-md-6 col-md-offset-4">
                                                         <button type="submit" class="btn btn-primary">
-                                                            Register
+                                                            Change Info
                                                         </button>
                                                     </div>
                                                 </div>
