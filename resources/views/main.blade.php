2906cc42efd39d0a0f1a679b917428f1bf6954b9
cruelka <kes1993@ya.ru>
2018-12-17 23:07:50 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -592,7 +592,7 @@
                                                                         hideSliderAtLimit: 0,
                                                                         hideCaptionAtLimit: 0,
                                                                         hideAllCaptionAtLilmit: 0,
-                                                                        debugMode: false,
+                                                                        debugMode: true,
                                                                         fallbacks: {
                                                                             simplifyAll: "off",
                                                                             nextSlideOnWindowFocus: "off",
