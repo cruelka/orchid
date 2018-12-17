2f7b5ebd68745a56fc07d339c9648e0d08dc49f9
cruelka <kes1993@ya.ru>
2018-12-17 23:06:04 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -521,7 +521,7 @@
                                                                     revapi2 = tpj("#rev_slider_2_1").show().revolution({
                                                                         sliderType: "standard",
                                                                         jsFileLocation: "//orchid.live/wp-content/plugins/revslider/public/assets/js/",
-                                                                        sliderLayout: "boxed",
+                                                                        sliderLayout: "full-width",
                                                                         dottedOverlay: "none",
                                                                         delay: 9000,
                                                                         navigation: {
