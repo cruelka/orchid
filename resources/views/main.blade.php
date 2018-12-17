c29e7e527bb9432368b7dcc3c0eda7c907245b92
cruelka <kes1993@ya.ru>
2018-12-17 23:17:41 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/web.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -521,7 +521,7 @@
                                                                     revapi2 = tpj("#rev_slider_2_1").show().revolution({
                                                                         sliderType: "standard",
                                                                         jsFileLocation: "//orchid.live/wp-content/plugins/revslider/public/assets/js/",
-                                                                        sliderLayout: "full-width",
+                                                                        sliderLayout: "auto",
                                                                         dottedOverlay: "none",
                                                                         delay: 9000,
                                                                         navigation: {
@@ -592,7 +592,7 @@
                                                                         hideSliderAtLimit: 0,
                                                                         hideCaptionAtLimit: 0,
                                                                         hideAllCaptionAtLilmit: 0,
-                                                                        debugMode: true,
+                                                                        debugMode: false,
                                                                         fallbacks: {
                                                                             simplifyAll: "off",
                                                                             nextSlideOnWindowFocus: "off",
@@ -629,6 +629,27 @@
                                                             }
                                                         </script>
                                                     </div><!-- END REVOLUTION SLIDER --></div>
+                                                <script type="text/javascript">function setREVStartSize(e) {
+                                                        try {
+                                                            var i = jQuery(window).width(), t = 9999, r = 0, n = 0, l = 0, f = 0, s = 0, h = 0;
+                                                            if (e.responsiveLevels && (jQuery.each(e.responsiveLevels, function (e, f) {
+                                                                        f > i && (t = r = f, l = e), i > f && f > r && (r = f, n = e)
+                                                                    }), t > r && (l = n)), f = e.gridheight[l] || e.gridheight[0] || e.gridheight, s = e.gridwidth[l] || e.gridwidth[0] || e.gridwidth, h = i / s, h = h > 1 ? 1 : h, f = Math.round(h * f), "fullscreen" == e.sliderLayout) {
+                                                                var u = (e.c.width(), jQuery(window).height());
+                                                                if (void 0 != e.fullScreenOffsetContainer) {
+                                                                    var c = e.fullScreenOffsetContainer.split(",");
+                                                                    if (c) jQuery.each(c, function (e, i) {
+                                                                        u = jQuery(i).length > 0 ? u - jQuery(i).outerHeight(!0) : u
+                                                                    }), e.fullScreenOffset.split("%").length > 1 && void 0 != e.fullScreenOffset && e.fullScreenOffset.length > 0 ? u -= jQuery(window).height() * parseInt(e.fullScreenOffset, 0) / 100 : void 0 != e.fullScreenOffset && e.fullScreenOffset.length > 0 && (u -= parseInt(e.fullScreenOffset, 0))
+                                                                }
+                                                                f = u
+                                                            } else void 0 != e.minHeight && f < e.minHeight && (f = e.minHeight);
+                                                            e.c.closest(".rev_slider_wrapper").css({height: f})
+                                                        } catch (d) {
+                                                            console.log("Failure at Presize of Slider:" + d)
+                                                        }
+                                                    }
+                                                    ;</script>
                                                 <div class="cms-info-plus class-position-absolute cms-info-plus info-plus-model bottom-content main_bg_color3 transparent section_padding_top_30 section_padding_bottom_30 table_section">
                                                     <div class="container">
                                                         <div class="row">
